617fe5fe3cbbeb35a68c594276b2e2bdbca55994
Aneree Shah <aneree.shah0209@gmail.com>
2020-01-29 00:42:15 +0530

    Updates

---
+++ precache-manifest.251085e7651254b666d8afb52656f339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15017b498815790947c63a4981ada76",
+    "url": "/pwaInReact/index.html"
+  },
+  {
+    "revision": "6f1d2f02155b94583879",
+    "url": "/pwaInReact/static/css/main.c0b5c7dc.chunk.css"
+  },
+  {
+    "revision": "eb4345d326e137c31ff9",
+    "url": "/pwaInReact/static/js/2.f1e01681.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pwaInReact/static/js/2.f1e01681.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f1d2f02155b94583879",
+    "url": "/pwaInReact/static/js/main.79cc8dfd.chunk.js"
+  },
+  {
+    "revision": "3dd075a0e09dbd20d2cb",
+    "url": "/pwaInReact/static/js/runtime-main.ec8d035e.js"
+  }
+]);
